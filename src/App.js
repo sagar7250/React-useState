b3c872533963ddbb8998d1df7a21bdf84332bea1
sagar7250 <hemasagar27521@gmail.com>
2024-10-04 09:51:43 +0530

    React Basic Projects

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'> <h1>
+      Hello World Welcome to react Js and Iam coming from App.js
+      </h1>
+    </div>
+  );
+}
+
+export default App;
